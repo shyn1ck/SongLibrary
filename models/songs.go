611894964d002f28054959d9612eca67efe6543c
shyn1ck <hy.parvizjon@gmail.com>
2024-09-30 19:04:05 +0500

    [refactor]: Setup project file structure

--- models/songs.go
+++ models/songs.go
@@ -6,8 +6,9 @@ import (
 
 type Song struct {
 	ID          uint      `json:"id" gorm:"primaryKey"`
-	Group       string    `json:"group"`
-	Title       string    `json:"title" gorm:"varchar(255);not null"`
+	ArtistID    uint      `json:"artist_id" gorm:"not null"`
+	Artist      Artist    `json:"artist" gorm:"foreignKey:ArtistID"`
+	Title       string    `json:"title" gorm:"type:varchar(255);not null"`
 	ReleaseDate time.Time `json:"release_date" gorm:"default:CURRENT_TIMESTAMP"`
 	Text        string    `json:"text" gorm:"type:text"`
 	Link        string    `json:"link" gorm:"type:text"`
